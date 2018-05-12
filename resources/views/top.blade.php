6553578dc37f238db89531d1253c9cca63525691
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-05-12 21:01:30 +0900

    top pageでblade foreachを使ってみるだけ

---
+++ resources/views/top.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+            }
+
+          </style>
+          </head>
+        <body>
+            <h1>top page </h1>
+            <?php $contents = [1,2,3]; ?>
+            @foreach ($contents as $content)
+             <p>{{ $content }}</p>
+            @endforeach
+        </body>
+    </html>
